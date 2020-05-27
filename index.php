aa49055af00f6935c4cdd7d72de621bcebf34d00
Sergey Klyuchnikov <bongoman@tut.by>
2020-05-27 20:33:30 +0300

    Add view

--- index.php
+++ index.php
@@ -3,15 +3,11 @@
 require_once __DIR__ . '/autoload.php';
 
 use App\Models\User;
-use App\Models\Foo;
-use App\Models\News;
 use App\Config;
 
 $config = Config::instance();
 
-//$newsList = News::findLastLines('3');
-//include 'App/viewNews.php';
 $view = new \App\View();
 $view->users = User::findAll(); 
-//$view->display(__DIR__  . '/App/templates/index.php');
+
 echo $view->render(__DIR__  . '/App/templates/index.php');
