c54f65cf0bcf73509d658f6854ddf8841c1dbe3c
Sergey Klyuchnikov <bongoman@tut.by>
2020-05-31 20:39:25 +0300

    add composer

--- App/MultiException.php
+++ App/MultiException.php
@@ -1,20 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace App;
 
-/**
- * Description of MultiException
- *
- * @author user
- */
 class MultiException 
-extends Exceptions
+extends \Exception
 implements \ArrayAccess, \Iterator
         {
     use TCollection;
